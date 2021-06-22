1e8fef8806897ac0e5322b31a2f86c0b59b2c25b
Wander Aquino <wander.aquino@gmail.com>
2021-06-22 00:44:20 -0300

    Separando o contexto de autenticação

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,71 @@
+import firebase from "firebase";
+import {createContext, ReactNode, useEffect, useState} from "react";
+import { firebaseAuth } from "../services/firebase";
+
+type AuthContextTypeProps = {
+    children: ReactNode;
+}
+
+type AuthContextType = {
+    user: User | undefined,
+    signInWithGoogle: () => void
+}
+  
+type User = {
+id: String,
+name: String,
+avatar: String
+}
+
+export const AuthContext = createContext({} as AuthContextType);
+
+export function AuthContextProvider(props: AuthContextTypeProps) {
+    const [user, setUser] = useState<User>();
+  
+    //Efeitos colaterais na aplicação - Hook do React.
+    useEffect(() => {
+      const unsubscribe = firebaseAuth.onAuthStateChanged(user => {
+        if(user) {
+          const {displayName, photoURL, uid} = user;
+          if (!displayName || !photoURL) {
+            throw new Error("Missing Information from Google Account.");
+          }
+          setUser({
+            id: uid,
+            name: displayName,
+            avatar: photoURL
+          });
+        }
+      })
+  
+      //Boa prática
+      return () => {
+        unsubscribe()
+      }
+    }, []);
+  
+    function signInWithGoogle()
+    {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      
+      firebaseAuth.signInWithPopup(provider).then(result => {
+          if (result.user) {
+              const {displayName, photoURL, uid} = result.user;
+              if (!displayName || !photoURL) {
+                throw new Error("Missing Information from Google Account.");
+              }
+              setUser({
+                id: uid,
+                name: displayName,
+                avatar: photoURL
+              });
+            }
+        })
+    }
+
+    return(
+    <AuthContext.Provider value={{user, signInWithGoogle}}>
+    {props.children}
+    </AuthContext.Provider>
+    );
+}
