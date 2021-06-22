497609a36daa0e4888f083c7cca8eacbe72f7073
Wander Aquino <wander.aquino@gmail.com>
2021-06-21 23:12:15 -0300

    Configurando as rotas no App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import {Home} from "./pages/Home"
+import {NewRoom} from "./pages/NewRoom";
+import {Home} from "./pages/Home";
+import {BrowserRouter, Route} from "react-router-dom";
+
 
 function App() {
-  return (<Home/>);
+  return (
+  <BrowserRouter>
+      <Route path="/" exact component={Home}/>
+      <Route path="/rooms/new" component={NewRoom}/>
+  </BrowserRouter>
+  );
 }
 
 export default App;
