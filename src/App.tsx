1a4d6ad17336082b6fd8c67148d64f0d4c9adf03
Wander Aquino <wander.aquino@gmail.com>
2021-06-22 00:45:21 -0300

    Atualizando os imports e removendo inutilizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import {NewRoom} from "./pages/NewRoom";
 import {Home} from "./pages/Home";
 import {BrowserRouter, Route} from "react-router-dom";
-
+import {AuthContextProvider} from "./contexts/AuthContext"
 
 function App() {
   return (
   <BrowserRouter>
-      <Route path="/" exact component={Home}/>
-      <Route path="/rooms/new" component={NewRoom}/>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home}/>
+        <Route path="/rooms/new" component={NewRoom}/>
+      </AuthContextProvider>
   </BrowserRouter>
   );
 }
