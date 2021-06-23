9eacd82d110b515025da60fd6969d7b2ea16e34f
Wander Aquino <wander.aquino@gmail.com>
2021-06-22 22:47:06 -0300

    Adicionando acesso para uma sala já criada a partir da home.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,14 @@ import  "../styles/auth.scss";
 
 import {Button} from "../components/Button";
 import { useAuth } from "../hooks/useAuths";
+import { FormEvent, useState } from "react";
+import { firebaseDb } from "../services/firebase";
 
 
 export function Home () {
     const history = useHistory();
     const {user, signInWithGoogle} = useAuth();
+    const [roomCode, setRoomCode] = useState("");
     
     function handleCreateRoom() {
         if(!user) {
@@ -18,7 +21,23 @@ export function Home () {
         }
 
         history.push("/rooms/new");
-    }
+    };
+
+    async function handleJoinRoom(event: FormEvent) {
+        event?.preventDefault();
+        if(roomCode.trim() === "") {
+            return;
+        };
+
+        const roomRef = await firebaseDb.ref(`/rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()) {
+            alert("Room not exists");
+            return;
+        };
+
+        history.push(`/rooms/${roomCode}`);
+    };
 
     return (
         <div id="page-auth">
@@ -35,10 +54,12 @@ export function Home () {
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                     <Button type="submit">Entrar na sala</Button>
                     </form>
